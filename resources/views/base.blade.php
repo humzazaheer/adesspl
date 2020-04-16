ad0f00740d52ba4b2b363136359d387cbd81a40a
Humza Zaheer <hamzazaheer165@gmail.com>
2020-04-16 14:18:48 +0500

    statics

---
+++ resources/views/base.blade.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet" type="text/css">
+    <link href="{{asset('css/mdb.min.css')}}" rel="stylesheet" type="text/css">
+    <link href="{{asset('css/style.css')}}" rel="stylesheet" type="text/css">
+    <link href="{{asset('css/global.css')}}" rel="stylesheet" type="text/css">
+    <link href="{{asset('fonts/font-awesome/css/all.css')}}" rel="stylesheet" type="text/css">
+    <link href="{{asset('css/lightslider.min.css')}}" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+    @yield('content')
+
+        {{--javascript files--}}
+        <script type="text/javascript" src="{{asset('js/jquery.min.js')}}"></script>
+        <script type="text/javascript" src="{{asset('js/bootstrap.min.js')}}"></script>
+        <script type="text/javascript" src="{{asset('js/mdb.min.js')}}"></script>
+        <script type="text/javascript" src="{{asset('js/popper.min.js')}}"></script>
+        <script type="text/javascript" src="{{asset('js/lightslider.min.js')}}"></script>
+        <script type="text/javascript" src="{{asset('js/script.js')}}"></script>
+        </body>
+</html>
