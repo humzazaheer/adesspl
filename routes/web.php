1bc3ad7e1d950153c0870858617ee1585535b19b
humzazaheer <hamzazaheer165@gmail.com>
2020-04-25 17:27:51 +0500

    update

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'PageController@index');
 Route::get('/about', 'PageController@about');
+Route::get('/contact', 'PageController@contact');
+
