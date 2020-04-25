173b7a2dca9ffcbe0025fe4be1d7e3499edd70d7
faiqueimtiaz <faiqueimtiaz123@gmail.com>
2020-04-25 17:30:15 +0500

    commit

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,8 +3,10 @@
 @section('content')
 <h1>About</h1>
 
-<button type="button" class="btn btn-primary" name="button">Test
-<i class="fad fa-hard-hat fa-3x"></i>
-</button>
+<div class="container">
+
+  
+
+</div>
 
 @endsection
