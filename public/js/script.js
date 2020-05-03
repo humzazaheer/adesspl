f2efab4e15b451b2cde557228e6f41018bd96652
humzazaheer <hamzazaheer165@gmail.com>
2020-05-03 23:57:21 +0500

    update

--- public/js/script.js
+++ public/js/script.js
@@ -3,4 +3,34 @@ $(document).ready(function () {
     $("#clientSlider").lightSlider({
         loop: true,
     });
+
+    var autoplaySlider = $('#lightSlider').lightSlider({
+        auto:true,
+        loop:true,
+        item:4,
+        thumbMargin: 3,
+        onBeforeSlide: function (el) {
+            $('#current').text(el.getCurrentSlideCount());
+        },
+
+        responsive : [
+            {
+                breakpoint:800,
+                settings: {
+                    item:3,
+                    slideMove:1,
+                    slideMargin:6,
+                }
+            },
+            {
+                breakpoint:480,
+                settings: {
+                    item:2,
+                    slideMove:1
+                }
+            }
+        ]
+    });
+    $('#total').text(autoplaySlider.getTotalSlideCount());
+
 });
--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -13,15 +13,71 @@
             </h2>
         </div>
     </section>
+    <div class="container">
+
+{{--        who we are?--}}
 
     <section class="aboutUs">
-
-        <div class="container">
             <h1 class="text-center mb-4">Who We Are?</h1>
             <p>ADESSPL is an engineering services, manufacturing and construction company op­erating in Pakistan. We are in the business of delivering client-specific solutions for projects related to Energy, Infrastructure and Other Process industry. The package of services encompasses engineering, procurement, manufacturing, commission­ing and maintenance. We are a superior company renowned in the region for its quality, safety and on-time delivery of projects and products. All the faculties required to deliver turnkey projects, reside within the company. This unique strength enables us to provide solutions as EPCC (Engineering, Procurement, Construction and Commissioning) services provider to a host of clients.
                 ADESSPL's employees qualified & highly experienced personnel are capable of or­ganizing, executing & handling of al kind of mechanics construction projects from start to completion. ADESSPL's design team is fully capable, competent of under­taking design, engineering of steel structures, Fabrication of any size & application.</p>
-            <img src="{{asset('images/1fcecab6-0e52-4c7c-b178-c468144bdee3.JPG')}}" class="img-fluid w-75 mx-auto d-block my-4 shadow"/>
+            <img src="{{asset('images/92c9b701-0f3d-42bc-823d-8d66e4c82918.JPG')}}" class="img-fluid w-75 mx-auto d-block my-4 shadow"/>
             <p>ADESSPL is an QMS ISO 9001:2015, EMS ISO 14001:2015 and OHSMS ISO 45001:2018 certified company, providing quality services in the area of project management, engineering, manufacturing and construction to domestic and over­seas clients. ADESSPL project teams have internationally operated in all avenues of project implementation as general construction contractor, maintenance turn­around planner and turnkey (EPC) solution provider. ADESSPL's personnel have a long & distinguished record of teamwork developed over year of association at vari­ous projects.</p>
-
-        </div>
     </section>
+
+{{--        statements--}}
+
+        <section class="statements">
+            <!-- Card group -->
+            <div class="card-group">
+
+
+
+                <!-- Card -->
+                <div class="card mb-6">
+
+                    <!-- Card image -->
+                    <div class="card-icon my-5 text-center text-danger">
+                        <i class="fad fa-bullseye-arrow fa-5x"></i>
+                    </div>
+
+                    <!-- Card content -->
+                    <div class="card-body">
+                        <!-- Title -->
+                        <h3 class="card-title text-center">Mission Statement</h3>
+                        <!-- Text -->
+                        <p class="card-text">To harnesses the synergy by the skills and expertise of ADESSPL in carrying out turnkey engineering projects and is committed to achieving customer satisfaction by providing quality products and services in a cost-effective and environment friendly manner.</p>
+
+                    </div>
+                    <!-- Card content -->
+
+                </div>
+                <!-- Card -->
+
+                <!-- Card -->
+                <div class="card mb-6">
+
+                    <!-- Card image -->
+                    <div class="card-icon my-5 text-center text-warning">
+                        <i class="fad fa-eye fa-5x"></i>
+                    </div>
+
+                    <!-- Card content -->
+                    <div class="card-body">
+
+                        <!-- Title -->
+                        <h3 class="card-title text-center">Vision Statement</h3>
+                        <!-- Text -->
+                        <p class="card-text">To become the preferred leading Contractor and Supplier to Petrochemical, Oil & Gas, fertilizer, Power Generation Cem ent and Industrial Projects in Asia. Customers and partners will see us as integral to their success. We will anticipate their needs and deliver on every commitment we make. People will be proud to work at ADESSPL. We will create opportunities to achieve the extraordinary, and we will reward
+                            success.</p>
+
+                    </div>
+                    <!-- Card content -->
+
+                </div>
+                <!-- Card -->
+
+            </div>
+            <!-- Card group -->
+        </section>
+    </div>
