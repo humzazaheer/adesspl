1da71e9bda466b34003b86ab0656835b23d7c972
humzazaheer <hamzazaheer165@gmail.com>
2020-05-03 19:58:57 +0500

    update

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -13,3 +13,15 @@
             </h2>
         </div>
     </section>
+
+    <section class="aboutUs">
+
+        <div class="container">
+            <h1 class="text-center mb-4">Who We Are?</h1>
+            <p>ADESSPL is an engineering services, manufacturing and construction company op­erating in Pakistan. We are in the business of delivering client-specific solutions for projects related to Energy, Infrastructure and Other Process industry. The package of services encompasses engineering, procurement, manufacturing, commission­ing and maintenance. We are a superior company renowned in the region for its quality, safety and on-time delivery of projects and products. All the faculties required to deliver turnkey projects, reside within the company. This unique strength enables us to provide solutions as EPCC (Engineering, Procurement, Construction and Commissioning) services provider to a host of clients.
+                ADESSPL's employees qualified & highly experienced personnel are capable of or­ganizing, executing & handling of al kind of mechanics construction projects from start to completion. ADESSPL's design team is fully capable, competent of under­taking design, engineering of steel structures, Fabrication of any size & application.</p>
+            <img src="{{asset('images/1fcecab6-0e52-4c7c-b178-c468144bdee3.JPG')}}" class="img-fluid w-75 mx-auto d-block my-4 shadow"/>
+            <p>ADESSPL is an QMS ISO 9001:2015, EMS ISO 14001:2015 and OHSMS ISO 45001:2018 certified company, providing quality services in the area of project management, engineering, manufacturing and construction to domestic and over­seas clients. ADESSPL project teams have internationally operated in all avenues of project implementation as general construction contractor, maintenance turn­around planner and turnkey (EPC) solution provider. ADESSPL's personnel have a long & distinguished record of teamwork developed over year of association at vari­ous projects.</p>
+
+        </div>
+    </section>
