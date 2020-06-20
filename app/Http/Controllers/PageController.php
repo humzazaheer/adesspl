b5f8f2ff985fcdd0769166bee703544e1600e5fe
faique <faiqueimtiaz123@gmail.com>
2020-06-20 22:51:24 +0500

    updates

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -46,4 +46,55 @@ class PageController extends Controller
     }
 
 
+    // Solution & services pages
+    public function turnkey_epc_project()
+    {
+        return view('turnkey_epc_project', []);
+    }
+    public function fabrication_and_plant_eraction()
+    {
+        return view('fabrication_and_plant_eraction', []);
+    }
+    public function installation_and_commissioning_of_plant_machinery()
+    {
+        return view('installation_and_commissioning_of_plant_machinery', []);
+    }
+    public function o_m_services()
+    {
+        return view('o_m_services', []);
+    }
+    public function manufacturing_of_machine_element()
+    {
+        return view('manufacturing_of_machine_element', []);
+    }
+    public function overhauling_of_diesel_engine_and_parts()
+    {
+        return view('overhauling_of_diesel_engine_and_parts', []);
+    }
+    public function design_engineering()
+    {
+        return view('design_engineering', []);
+    }
+    public function project_management_consultancy()
+    {
+        return view('project_management_consultancy', []);
+    }
+    public function plant_maintenance_and_turnaround()
+    {
+        return view('plant_maintenance_and_turnaround', []);
+    }
+    public function infrastructure_and_mep()
+    {
+        return view('infrastructure_and_mep', []);
+    }
+    public function manpower_supply()
+    {
+        return view('manpower_supply', []);
+    }
+    public function equipment_on_rent()
+    {
+        return view('equipment_on_rent', []);
+    }
+
+
 }
--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -15,61 +15,91 @@
     </section>
     <div class="container">
 
-        <section class="project-gallery text-center">
+        <section class="gallery text-center">
             <h1 class="text-center mb-4 ad-black">Projects Lightbox</h1>
             <p>Check out our works gallery.</p>
 
             <div class="row">
+                <div class=" col-lg-4 col-md-6">
+                    <div class="card card-cascade wider mt-2 mb-3 ">
+
+                        <!-- Card image -->
+
+                        <div class="view view-cascade overlay">
+                            <img class="card-img-top" src="http://127.0.0.1:8000/images/92c9b701-0f3d-42bc-823d-8d66e4c82918.JPG" alt="Card image cap">
+                            <a href="project_gallery">
+                                <div class="mask rgba-white-slight waves-effect waves-light"></div>
+                            </a>
+                        </div>
+
+                        <!-- Card content -->
+                        <div class="card-body card-body-cascade text-center">
+
+                            <!-- Title -->
+                            <h4 class="card-title"><strong>Project Title</strong></h4>
 
 
+                            <!-- Text -->
+                            <a href="project_gallery" class="btn btn-rounded ad-orangeGradient font-weight-bold">See More</a>
+                        </div>
 
+                    </div>
 
-                <figure class="col-md-4  view">
-                    <a href="{{url('/project_gallery')}}" data-size="1600x1067">
-                        <img src="{{asset('images/umr.JPG')}}"
-                             alt="placeholder"
-                             class="img-fluid  ">
-                    </a>
-
-                </figure>
-
-                <figure class="col-md-4 view overlay  ">
-                    <a href="{{url('/project_gallery')}}" data-size="1600x1067">
-                        <img src="{{asset('images/umr.JPG')}}"
-                             alt="placeholder"
-                             class="img-fluid"
-                        >
-                    </a>
-                </figure> <figure class="col-md-4 view overlay  ">
-                    <a href="{{url('/project_gallery')}}" data-size="1600x1067">
-                        <img src="{{asset('images/umr.JPG')}}"
-                             alt="placeholder"
-                             class="img-fluid"
-                        >
-                    </a>
-                </figure> <figure class="col-md-4 view overlay  ">
-                    <a href="{{url('/project_gallery')}}" data-size="1600x1067">
-                        <img src="{{asset('images/umr.JPG')}}"
-                             alt="placeholder"
-                             class="img-fluid"
-                        >
-                    </a>
-                </figure> <figure class="col-md-4 view overlay  ">
-                    <a href="{{url('/project_gallery')}}" data-size="1600x1067">
-                        <img src="{{asset('images/umr.JPG')}}"
-                             alt="placeholder"
-                             class="img-fluid"
-                        >
-                    </a>
-                </figure> <figure class="col-md-4 view overlay  ">
-                    <a href="{{url('/project_gallery')}}" data-size="1600x1067">
-                        <img src="{{asset('images/umr.JPG')}}"
-                             alt="placeholder"
-                             class="img-fluid"
-                        >
-                    </a>
-                </figure>
                 </div>
+                <div class=" col-lg-4 col-md-6">
+                    <div class="card card-cascade wider mt-2 mb-3 ">
+
+                        <!-- Card image -->
+
+                        <div class="view view-cascade overlay">
+                            <img class="card-img-top" src="http://127.0.0.1:8000/images/92c9b701-0f3d-42bc-823d-8d66e4c82918.JPG" alt="Card image cap">
+                            <a href="project_gallery">
+                                <div class="mask rgba-white-slight waves-effect waves-light"></div>
+                            </a>
+                        </div>
+
+                        <!-- Card content -->
+                        <div class="card-body card-body-cascade text-center">
+
+                            <!-- Title -->
+                            <h4 class="card-title"><strong>Project Title</strong></h4>
+
+
+                            <!-- Text -->
+                            <a href="project_gallery" class="btn btn-rounded ad-orangeGradient font-weight-bold">See More</a>
+                        </div>
+
+                    </div>
+
+                </div>
+                <div class=" col-lg-4 col-md-6">
+                    <div class="card card-cascade wider mt-2 mb-3 ">
+
+                        <!-- Card image -->
+
+                        <div class="view view-cascade overlay">
+                            <img class="card-img-top" src="http://127.0.0.1:8000/images/92c9b701-0f3d-42bc-823d-8d66e4c82918.JPG" alt="Card image cap">
+                            <a href="project_gallery">
+                                <div class="mask rgba-white-slight waves-effect waves-light"></div>
+                            </a>
+                        </div>
+
+                        <!-- Card content -->
+                        <div class="card-body card-body-cascade text-center">
+
+                            <!-- Title -->
+                            <h4 class="card-title"><strong>Project Title</strong></h4>
+
+
+                            <!-- Text -->
+                            <a href="project_gallery" class="btn btn-rounded ad-orangeGradient font-weight-bold">See More</a>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+
 
         </section>
     </div>
