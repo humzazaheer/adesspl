21348b9c997218dfbbd01789eb3e0d878a7c279d
humzazaheer <hamzazaheer165@gmail.com>
2020-09-12 20:51:40 +0500

    update

--- resources/views/admin/base.blade.php
+++ resources/views/admin/base.blade.php
@@ -9,6 +9,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
+    <link rel="icon" type="image/png" href="{{asset('images/logo.png')}}">
     <link rel="stylesheet" type="text/css" href="{{asset('/css/global.css')}}">
     <link rel="stylesheet" type="text/css" href="{{asset('/css/admin.css')}}">
     <link rel="stylesheet" type="text/css" href="{{asset('/css/bootstrap.min.css')}}">
--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -6,6 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
+    <link rel="icon" type="image/png" href="{{asset('images/logo.png')}}">
     <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet" type="text/css">
     <link href="{{asset('css/mdb.min.css')}}" rel="stylesheet" type="text/css">
     <link href="{{asset('css/style.css')}}" rel="stylesheet" type="text/css">
