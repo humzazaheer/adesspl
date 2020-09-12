911d3a0123f4dc50be456ab86369be44512cd51c
Faique Imtiaz <59650015+faiqueimtiaz@users.noreply.github.com>
2020-09-12 12:12:14 +0500

    Add files via upload

--- resources/views/qhse.blade.php
+++ resources/views/qhse.blade.php
@@ -8,7 +8,8 @@
             <h2>
                 <span class="banner-icon"><img src="{{asset('images/engineer-80px.png')}}" alt=""></span>
                 <br>
-                QHSE Policy
+                QUALITY POLICY
+
             </h2>
         </div>
     </section>
@@ -19,13 +20,13 @@
         <section class="dark-grey-text text-center qhse-section">
 
             <!-- Section heading -->
-            <h2 class="text-center mb-4 wow fadeInDown">Quality | Health | Safety | Environment</h2>
-            <!-- Section description -->
-            <p class="wow fadeInUp mb-5">ADESSPL is an <strong>QMS ISO 9001:2015, EMS ISO 14001:2015 and OHSMS ISO 45001:2018 </strong>certified company, providing quality services in the area of project management,
-                engineering, manufacturing and construction to domestic and over­seas clients.
+            <h2 class="text-center mb-4 wow fadeInDown">QUALITY AT ADESSPL</h2>
 
-                ADESSPL strategy goes around Plan, Do, Check and Act <strong class="font-weight-bold">(PDCA Cycle)</strong>
-                and is reflected in all our ayatems eith the following key elements:
+
+            <!-- Section description -->
+            <p class="wow fadeInUp mb-5">Quality is the most important aspect of our business for the satisfaction of our customers & clients. Our
+                        strategy goes around Plan, Do, Check & Act (PDCA Cycle) and is reflected in all our systems with the
+                        following key elements:
             </p>
 
             <!-- Grid row -->
@@ -41,8 +42,9 @@
                         <div class="col-12 wow fadeInLeft">
                             <i class="fad fa-2x fa-universal-access mb-2 text-danger"></i>
                             <h5 class="font-weight-bold mb-3">Unserstand & Comply</h5>
-                            <p class=" ">Understand and comply with applicable legal regulatory and statutory standards
-                                and other requirements related to our business.
+                            <p class=" ">Understand and comply with applicable legal, regulatory & statutory standards and other
+                                        requirements related to our business.
+
                             </p>
                         </div>
                         <!-- Grid column -->
@@ -55,9 +57,10 @@
 
                         <div class="col-12 wow fadeInLeft">
                             <i class="fas fa-2x fa-users mb-2 text-warning"></i>
-                            <h5 class="font-weight-bold mb-3">A Business Family</h5>
-                            <p class=" ">Making clients contractors/subcontractors and business partners familier with our QHSE system
-                                and creating awareness on how their actions can influence company's QHSE Performance.</p>
+                            <h5 class="font-weight-bold mb-3">Operating the business legal</h5>
+                            <p class=" ">Operating the business to the systems required by QMS ISO 9001:2015, EMS ISO 14001:2015 and
+                                        OHSMS ISO 45001:2018.
+                            </p>
                         </div>
                         <!-- Grid column -->
 
@@ -70,10 +73,10 @@
                         <!-- Grid column -->
                         <div class="col-12 wow fadeInLeft">
                             <i class="fad fa-2x fa-network-wired mb-2 text-danger"></i>
-                            <h5 class="font-weight-bold mb-3">Identify Hurdles</h5>
-                            <p class="  mb-3">All hazards relevent to our activities are identified,
-                                accessed and controlled to ensure the health safety, environment and
-                                welfare of employees and others who may be effected by our work.
+                            <h5 class="font-weight-bold mb-3">Making client, contractors & partners</h5>
+                            <p class="  mb-3">Making client, contractors/sub-contractors and business partners, familiar with our quality
+                                management system and creating awareness on how their actions can influence the company's
+                                quality management system.
                             </p>
                         </div>
                         <!-- Grid column -->
@@ -85,9 +88,11 @@
                         <!-- Grid column -->
                         <div class="col-12 wow fadeInLeft">
                             <i class="fad fa-2x fa-skiing-nordic mb-2 text-warning"></i>
-                            <h5 class="font-weight-bold mb-3">Take a Lead</h5>
-                            <p class="  mb-0">Take lead in control and mitigating potential risks associated with
-                                people, land , air, ecology and assets.</p>
+                            <h5 class="font-weight-bold mb-3">Strive for improvement</h5>
+                            <p class="  mb-0">Continually strive for improvement by providing adequate trainings, motivation and promoting quality
+                            awareness to our workforce in order to improve their performance and deliver a best class quality
+                            services to our valued customer.
+                                </p>
                         </div>
                         <!-- Grid column -->
 
@@ -114,9 +119,9 @@
 
                         <div class="col-12 wow fadeInRight">
                             <i class="fad fa-2x fa-award mb-2 text-warning"></i>
-                            <h5 class="font-weight-bold mb-3">Achieve Quality</h5>
-                            <p class=" ">Make and achieve quality and HSE Objectives at all levels of
-                                company to become one of the benchmarks QHSE organiztion </p>
+                            <h5 class="font-weight-bold mb-3">Make and achieve quality</h5>
+                            <p class=" ">Make and achieve quality objectives at all level of company.
+                                </p>
                         </div>
                         <!-- Grid column -->
 
@@ -129,62 +134,17 @@
 
                         <div class="col-12 wow fadeInRight">
                             <i class="fad fa-2x fa-check-double mb-2 text-danger"></i>
-                            <h5 class="font-weight-bold mb-3">Systematic & Measurable Approach</h5>
-                            <p class=" ">Apply systematic & measurable approach to continually improve upon our QHSE cultue &
-                                performance.
+                            <h5 class="font-weight-bold mb-3">Face the challenges</h5>
+                            <p class=" ">Face the challenges of changing market environment through our proper communication and
+                                        commitment within the organization.
+
                             </p>
                         </div>
                         <!-- Grid column -->
 
                     </div>
 
-                    <div class="row mb-md-0 mb-3">
-
-                        <!-- Grid column -->
-                        <div class="col-12 wow fadeInRight">
-                            <i class="fad fa-2x fa-hand-holding-magic mb-2 text-warning"></i>
-                            <h5 class="font-weight-bold mb-3">Everything is Preventable</h5>
-                            <p class="mb-3">
-                                We believe that all accidents are preventable. If we continuously strive
-                                for zero defects preferably through proactive measures.
-                            </p>
-                        </div>
-                        <!-- Grid column -->
-
-                    </div>
-
-                    <div class="row">
-
-
-                        <!-- Grid column -->
-                        <div class="col-12 mt-5 wow fadeInRight">
-                            <i class="fal fa-2x fa-comment-alt-smile mb-2 text-danger"></i>
-
-                            <h5 class="font-weight-bold mb-3">Communicate with Stakeholders</h5>
-                            <p class="  mb-md-0">
-                                Communicate this policy with stakeholders and encourage them to participate
-                                in qhse process performance improvement initiatives and rewards individual
-                                and team performance consistantly.
-                            </p>
-                        </div>
-                        <!-- Grid column -->
-
-                    </div>
-                    <!-- Grid row -->
-
-                </div>
-                <!-- Grid column -->
-
-            </div>
-            <!-- Grid row -->
-            <div class="qhse-last d-inline-block wow fadeInUp">
-                <i class="fas fa-2x fa-handshake-alt mb-2 text-warning"></i>
-
-                <h5 class="font-weight-bold mb-3">ADESSPL Contribution</h5>
-                <p class="  mb-md-0">
-                    Living by the value of ADESSPL we contribute in culture development through team works and active participation.
-                </p>
-            </div>
+                  
         </section>
         <!--Section: Content-->
 
